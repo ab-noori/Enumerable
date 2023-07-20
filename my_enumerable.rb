952fbc7bcbbb11d53b3318f60da358a7d228706a
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-07-20 17:06:11 +0330

    Add my_enumerable.rb file.

---
+++ my_enumerable.rb
@@ -0,0 +1,13 @@
+module MyEnumerable
+  def map
+    # Implementation of map method
+  end
+
+  def filter
+    # Implementation of filter method
+  end
+
+  def reduce
+    # Implementation of reduce method
+  end
+end
